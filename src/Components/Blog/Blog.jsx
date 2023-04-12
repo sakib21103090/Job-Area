8661e35d804a8b1d228560320343cd0b47e29210
sakib21103090 <21103090@iubat.edu>
2023-04-12 16:20:44 +0600

    add Q 3

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -9,7 +9,7 @@ const Blog = () => {
             </h3>
 
             <h4>
-                
+           Q: 3 Refs is the shorthand used for references in React. It is similar to keys in React. It is an attribute which makes it possible to store a reference to particular DOM nodes or React elements. It provides a way to access React DOM nodes or React elements and how to interact with it.
             </h4>
         </div>
     );
