76f4fe2382a514f4c4b1df1cb0b5e08b1a99fdbe
sakib21103090 <21103090@iubat.edu>
2023-04-12 16:19:40 +0600

    add Q and 1 2

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h3>blog</h3>
+            <h4> Q:1 The React Context API was introduced to overcome the problem of passing props down the tree of components. The state of the application can be stored globally, and shared across multiple components. Now with the addition of hooks to the React architecture, we get a new hook called useContext</h4>
+            <h3>
+           Q:2 A custom hook is a special JavaScript function whose name starts with 'use' and can be used to call other hooks.
+            </h3>
+
+            <h4>
+                
+            </h4>
         </div>
     );
 };
